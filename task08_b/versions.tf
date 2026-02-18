52b61f37f4de81a03a78af89bb9e03848716ae77
Giorgi Makharadze <giorgimakharadze4@gmail.com>
2026-02-18 20:19:43 +0400

    idk

--- task08_b/versions.tf
+++ task08_b/versions.tf
@@ -36,12 +36,14 @@ provider "azurerm" {
 provider "kubectl" {
   host                   = module.aks.kube_config_structured[0].host
   cluster_ca_certificate = base64decode(module.aks.kube_config_structured[0].cluster_ca_certificate)
-  token                  = module.aks.kube_config_structured[0].token
+  client_certificate     = base64decode(module.aks.kube_config_structured[0].client_certificate)
+  client_key             = base64decode(module.aks.kube_config_structured[0].client_key)
   load_config_file       = false
 }
 
 provider "kubernetes" {
   host                   = module.aks.kube_config_structured[0].host
   cluster_ca_certificate = base64decode(module.aks.kube_config_structured[0].cluster_ca_certificate)
-  token                  = module.aks.kube_config_structured[0].token
+  client_certificate     = base64decode(module.aks.kube_config_structured[0].client_certificate)
+  client_key             = base64decode(module.aks.kube_config_structured[0].client_key)
 }
