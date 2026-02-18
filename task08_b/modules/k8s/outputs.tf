55f121f32ea1700e590c4fa51dfd4e587d60232c
Giorgi Makharadze <giorgimakharadze4@gmail.com>
2026-02-18 21:21:15 +0400

    idk

--- task08_b/modules/k8s/outputs.tf
+++ task08_b/modules/k8s/outputs.tf
@@ -1,4 +1,4 @@
 output "service_ip" {
-  value       = data.kubernetes_service_v1.app_service.status[0].load_balancer[0].ingress[0].ip
+  value       = try(data.kubernetes_service_v1.app_service.status[0].load_balancer[0].ingress[0].ip, "pending")
   description = "Load Balancer IP"
 }
