8f55773cf0dcaad78925d30d7b3f6b4a9be94f56
Giorgi Makharadze <giorgimakharadze4@gmail.com>
2026-02-19 11:56:20 +0400

    fixed name

--- task08_b/locals.tf
+++ task08_b/locals.tf
@@ -2,7 +2,7 @@ locals {
   name_prefix    = var.resources_name_prefix
   aca_name       = "${local.name_prefix}-ca"
   aca_env_name   = "${local.name_prefix}-cae"
-  acr_name       = replace("${local.name_prefix}acr", "-", "")
+  acr_name       = replace("${local.name_prefix}cr", "-", "")
   aks_name       = "${local.name_prefix}-aks"
   keyvault_name  = "${local.name_prefix}-kv"
   redis_aci_name = "${local.name_prefix}-redis-ci"
